48a4ac5badcc01a1e287c81ceca87de8d0737cf4
elen-afipe <190838117+elen-afipe@users.noreply.github.com>
2025-05-28 17:59:37 +0300

    Create Ship class with tests

---
+++ tests/ship.test.js
@@ -0,0 +1,14 @@
+import {Ship} from "../game-modules/ship.js"
+
+test('ship allows to create a carrier with length 5 and 0 hits', ()=>{
+    const Carrier = new Ship(5)
+    expect(Carrier.length).toBe(5),
+    expect(Carrier.timesHit).toBe(0)
+})
+
+test('when destroyer is hit 2 times, it  is sunk', ()=>{
+    const Destroyer = new Ship(2)
+    Destroyer.hit();
+    Destroyer.hit();
+    expect(Destroyer.sunk).toBe(true)
+})
